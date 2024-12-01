151287a63eb291364f88a5422f8d1021e5f1c18a
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2024-12-01 05:28:27 +0100

    css and html works

--- app_fun/settings.py
+++ app_fun/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'app_fun.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'clock/../templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- clock/urls.py
+++ clock/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
+import clock
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.clock, name="clock"),
-]
+    path('', RedirectView.as_view(url='/clock/')),
+    path('clock/', views.clock, name='clock'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
